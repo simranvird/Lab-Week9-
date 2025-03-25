c07ba325f97caaa00cc8b1f11808663e1619e65e
Simranpreet Kaur <169073573+simranvird@users.noreply.github.com>
2025-03-24 22:54:01 -0400

    Main.java

--- src/main/java/org/example/assignment2/Main.java
+++ src/main/java/org/example/assignment2/Main.java
@@ -1,4 +1,29 @@
 package org.example.assignment2;
 
-public class main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Label label = new Label("Hello, JavaFX!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 250); // Set window size to 300x250
+        primaryStage.setTitle("JavaFX Skeleton");
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
